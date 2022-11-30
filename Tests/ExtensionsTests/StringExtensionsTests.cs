b5c4889b9a0967c9d40e1dc44f9fafe618ef47a1
Vlad Antonenko <vantonenko@sdl.com>
2022-11-30 22:10:15 +0200

    * more tests

--- Tests/ExtensionsTests/StringExtensionsTests.cs
+++ Tests/ExtensionsTests/StringExtensionsTests.cs
@@ -10,7 +10,7 @@ namespace LightTrace.Tests.ExtensionsTests
         [TestCase("123", 10, "123")]
         [TestCase("123", 0, "")]
         [TestCase("123", 2, "12")]
-        public void TakeFirst_Empty_Success(string input, int count, string expectedOutput) =>
-            Assert.That(input.TakeFirst(count), Is.EqualTo(expectedOutput));
+        public void TakeFirst_Success(string input, int count, string expected) =>
+            Assert.That(input.TakeFirst(count), Is.EqualTo(expected));
     }
 }
---
+++ Tests/ExtensionsTests/TimeSpanExtensionsTests.cs
@@ -0,0 +1,24 @@
+using LightTrace.Extensions;
+
+namespace LightTrace.Tests.ExtensionsTests;
+
+public class TimeSpanExtensionsTests
+{
+    [Test]
+    [TestCase(0, 0, 0)]
+    [TestCase(1, 2, 1)]
+    [TestCase(2, 1, 1)]
+    public void Min_Success(int aTicks, int bTicks, int expectedTicks) =>
+        Assert.That(
+            new TimeSpan(aTicks).Min(new TimeSpan(bTicks)), 
+            Is.EqualTo(new TimeSpan(expectedTicks)));
+
+    [Test]
+    [TestCase(0, 0, 0)]
+    [TestCase(1, 2, 2)]
+    [TestCase(2, 1, 2)]
+    public void Max_Success(int aTicks, int bTicks, int expectedTicks) =>
+        Assert.That(
+            new TimeSpan(aTicks).Max(new TimeSpan(bTicks)), 
+            Is.EqualTo(new TimeSpan(expectedTicks)));
+}
