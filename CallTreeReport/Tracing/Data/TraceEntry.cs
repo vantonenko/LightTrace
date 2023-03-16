c6adb23d2aaf0b54beaaeb1538f2569304ef27a9
vantonenko <vladislav.antonenko@gmail.com>
2023-03-16 16:13:05 +0200

    Feature/initial (#5)
    * * initial commit
    
    * * not yet documented
    
    * * first tests
    
    * * Arrange folders
    
    * * more tests
    
    * * more tests
    
    * * More tests, minor improvements
    
    * * Move MultiThreadedFlatReport tracing into a separate folder
    
    * * Call tree report tracing (single threaded for the moment)
    
    * * Add multi-threading support
    
    * Stopped the StopWatch before adding data to the dictionary (#2)
    
    Co-authored-by: Serhii Kurylchyk <skurylchyk@sdl.com>
    
    * Feature/thread safe tree (#4)
    
    * * use a common three across all the threads
    * thread-safe node insert still to be fixed
    
    * * insert new node thread safely
    
    * * better formatted output
    
    * * This isn't a single threaded anymore
    
    * * multi-threaded smoke test
    
    * * Use AsyncLocal
    
    * * Simplify it a bit
    
    * * Simplify a bit
    
    * * add reporting into the temp folder
    
    ---------
    
    Co-authored-by: Vlad Antonenko <vantonenko@sdl.com>
    
    ---------
    
    Co-authored-by: Vlad Antonenko <vantonenko@sdl.com>
    Co-authored-by: BlackPower <1130357+BlackPower@users.noreply.github.com>
    Co-authored-by: Serhii Kurylchyk <skurylchyk@sdl.com>

---
+++ CallTreeReport/Tracing/Data/TraceEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2.Tracing.Data;
+
+internal class TraceEntry
+{
+    public int Count;
+    
+    public long Ticks = 0;
+
+    public TimeSpan TimeSpan => TimeSpan.FromTicks(Ticks);
+    
+    public TraceEntries TraceEntries { get; set; } = new();
+}
+
---
+++ MultiThreadedFlatReport/Tests/ExtensionsTests/StringExtensionsTests.cs
@@ -0,0 +1,16 @@
+using LightTrace.Extensions;
+
+namespace LightTrace.Tests.ExtensionsTests
+{
+    public class StringExtensionsTests
+    {
+        [Test]
+        [TestCase("", 0, "")]
+        [TestCase("", 10, "")]
+        [TestCase("123", 10, "123")]
+        [TestCase("123", 0, "")]
+        [TestCase("123", 2, "12")]
+        public void TakeFirst_Success(string input, int count, string expected) =>
+            Assert.That(input.TakeFirst(count), Is.EqualTo(expected));
+    }
+}
